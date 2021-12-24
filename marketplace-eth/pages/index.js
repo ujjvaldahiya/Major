aa82d1a50597fcc32f6c4ff0050e235338b7d775
ujjvaldahiya <ujjvaldahiya786@gmail.com>
2021-12-25 03:01:13 +0530

    Commit 8 - Chain Id fix,  Card Customisations

--- marketplace-eth/pages/index.js
+++ marketplace-eth/pages/index.js
@@ -2,12 +2,20 @@ import { Hero } from "@components/ui/common"
 import { CourseList } from "@components/ui/course"
 import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
+import { CourseCard } from "@components/ui/course"
 
 export default function Home({courses}) {
   return (
     <>
       <Hero />
-      <CourseList courses={courses} />
+      <CourseList courses={courses}>
+        {
+          course => <CourseCard
+          key={course.id} 
+          course={course} 
+          />
+        }
+      </CourseList>
     </>
   )
 }
--- marketplace-eth/pages/marketplace/index.js
+++ marketplace-eth/pages/marketplace/index.js
@@ -3,17 +3,28 @@ import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
 import { WalletBar } from "@components/providers/web3"
 import { useAccount } from "@components/hooks/web3/useAccount"
+import { useNetwork } from "@components/hooks/web3/useNetwork"
+import { CourseCard } from "@components/ui/course"
 
 export default function Marketplace({courses}) {
-    const { account } = useAccount() 
+    const { account } = useAccount()
+    const { network } = useNetwork() 
     return (
         <>
         <div className="py-4">
             <WalletBar 
                 address={account.data} 
+                network={network.data}
             />
         </div>
-        <CourseList courses={courses} />
+        <CourseList courses={courses}>
+          {
+            course => <CourseCard
+            key={course.id} 
+            course={course} 
+            />
+          }
+        </CourseList>  
         </>
   )
 }
