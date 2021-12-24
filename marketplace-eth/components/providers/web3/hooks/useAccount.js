a9f8ea9e30a013516e077ba41f2076d5a7244ab2
ujjvaldahiya <ujjvaldahiya786@gmail.com>
2021-12-24 23:50:02 +0530

    Commit 6 - Added accounts, admin account, admin account hash

--- marketplace-eth/components/providers/web3/hooks/useAccount.js
+++ marketplace-eth/components/providers/web3/hooks/useAccount.js
@@ -1,15 +1,34 @@
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
+import useSWR from "swr"
 
-export const handler = web3 => () => {
-    const [account, setAccount] = useState(null)
+const adminAddresses = {
+    "0xcf226b732aaba06b5491a06679bec7004cfc754d71df4a53b1648645f14e15e5": true
+}
+
+export const handler = (web3, provider) => () => {
+    const {data, mutate, ...rest} = useSWR(() => 
+        web3 ? "web3/accounts" : null,
+        async () => {
+            const accounts = await web3.eth.getAccounts()
+            return accounts[0]
+        }
+    )
 
     useEffect(() => {
-        const getAccount = async () => {
-            const accounts = await web3.eth.getAccounts()
-            setAccount(accounts[0])
-        }
-        web3 && getAccount()
-    }, [web3])
+        provider&&
+        provider.on("accountsChanged", 
+        accounts => mutate(accounts[0] ?? null))
+    }, [provider])
 
-    return { account }
-}
+    return { 
+        account: {
+            data,
+            isAdmin: (
+                data && 
+                adminAddresses[web3.utils.keccak256(data)]) 
+                ?? false,
+            mutate, 
+            ...rest
+        } 
+    }
+} 
--- marketplace-eth/components/ui/common/navbar/index.js
+++ marketplace-eth/components/ui/common/navbar/index.js
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { useWeb3 } from "@components/providers"
 import { Button } from ".."
-import { useAccount } from "@components/web3/hooks/useAccount"
+import { useAccount } from "@components/hooks/web3/useAccount"
 
 export default function Footer() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3()
@@ -9,7 +9,6 @@ export default function Footer() {
 
   return (
       <section>
-        { account }
         <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
             <div className="flex justify-between items-center">
@@ -52,11 +51,11 @@ export default function Footer() {
                 onClick={connect}>
                   Loading...
               </Button> : isWeb3Loaded ?
-                account ?
+                account.data ?
                 <Button 
                   hoverable={false}
                   className="cursor-default">
-                  Hi there...
+                  Hi there {account.isAdmin && "Admin"}
                 </Button> :
                 <Button
                   onClick={connect}>
@@ -71,6 +70,13 @@ export default function Footer() {
           </div>
         </nav>
       </div>
+      { account.data &&
+      <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
+        <div className="text-white bg-indigo-600 rounded-md p-2">
+        { account.data }
+        </div>
+      </div>
+      }
     </section>
   )
 }
