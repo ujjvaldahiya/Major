6c79b9ee5981acbf782729a71ae98ea8bbb653db
ujjvaldahiya <ujjvaldahiya786@gmail.com>
2022-01-03 19:32:37 +0530

    Commit 17 - Truffle Contract fix 3

--- marketplace-eth/components/providers/web3/index.js
+++ marketplace-eth/components/providers/web3/index.js
@@ -2,6 +2,7 @@ import detectEthereumProvider from "@metamask/detect-provider"
 import Web3 from "web3"
 import React, { useState, useEffect, useMemo } from "react"
 import { setupHooks } from "./hooks/setupHooks"
+import { loadContract } from "@utils/loadContract"
 
 const {createContext, useContext} = require("react")
 
@@ -22,10 +23,12 @@ export default function Web3Provider({children}) {
         const provider = await detectEthereumProvider()
         if (provider) {
             const web3 = new Web3(provider)
+            const contract = await loadContract("ItemMarketPlace", web3)
+            console.log(contract)
             setWeb3Api({
               provider,
               web3,
-              contract: null,
+              contract,
               isLoading: false,
               hooks: setupHooks(web3, provider)
             })
