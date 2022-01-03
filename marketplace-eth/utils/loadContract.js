886843b9086983f4e6d38a4fb011cc8d7716d717
ujjvaldahiya <ujjvaldahiya786@gmail.com>
2022-01-03 19:34:09 +0530

    Commit 17 - Truffle Contract fix 3.1

--- marketplace-eth/utils/loadContract.js
+++ marketplace-eth/utils/loadContract.js
@@ -16,20 +16,3 @@ export const loadContract = async (name, web3) => {
 
     return contract
 }
-
-/*export const loadContract = async (name, provider) => {
-    const res = await fetch(`/contracts/${name}.json`)
-    const Artifact = await res.json()
-
-    const _contract = window.TruffleContract(Artifact)
-    _contract.setProvider(provider)
-
-    let deployedContract = null
-    try {
-        deployedContract = await _contract.deployed()
-    } catch {
-        console.log(`Contract ${name} cannot be loaded.`)
-    }
-
-    return deployedContract
-}*/
